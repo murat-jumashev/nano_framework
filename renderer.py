576e4291ccc900b923258bb9a5c70ce30f8ad603
jumasheff <jumasheff@gmail.com>
2018-02-24 16:42:09 +0600

    Extend render() function

--- renderer.py
+++ renderer.py
@@ -1,6 +1,12 @@
-def render(file_name):
+from jinja2 import Template
+
+def render(file_name, context=None):
     with open(file_name, 'r') as html_file:
         html = html_file.read()
+        if context:
+            template = Template(html)
+            # context может быть таким: {'some_var':'some_value'}
+            html = template.render(context)
         return html
 
 
